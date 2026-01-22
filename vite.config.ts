2b1c7806846900baf0bcf9d75c74602fa7332edc
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-01-22 23:38:59 +0600

    setup relative path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@/components": path.resolve(__dirname, "./src/components"),
+      "@/features": path.resolve(__dirname, "./src/features"),
+      "@/shared": path.resolve(__dirname, "./src/shared"),
+      "@/pages": path.resolve(__dirname, "./src/pages"),
+      "@/layouts": path.resolve(__dirname, "./src/layouts"),
+      "@/providers": path.resolve(__dirname, "./src/providers"),
+      "@/app": path.resolve(__dirname, "./src/app"),
+      "@/config": path.resolve(__dirname, "./src/config"),
+      "@/routes": path.resolve(__dirname, "./src/routes"),
+      "@tests": path.resolve(__dirname, "./tests"),
+    },
+  },
 });
