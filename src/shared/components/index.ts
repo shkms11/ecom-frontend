45fe52019e7888acbd840584ff76531eb508d0ba
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-01-23 23:59:16 +0600

    add barrel for shared/components

---
+++ src/shared/components/index.ts
@@ -0,0 +1,5 @@
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { Loader } from "./Loader";
+export { Toast } from "./Toast";
+export { PasswordInput } from "./PasswordInput";
