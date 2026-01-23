e9e80253d0b4c4de3a356e5096e86e7d32a5a95f
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-01-23 09:01:46 +0600

    wrap App with Redux, Router and ToastProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "@/app/store";
+import { AppRoutes } from "@/routes/AppRoutes";
+import { useToastContext } from "@/providers/toast/useToastContext";
+import { ToastProvider } from "./providers/toast/ToastProvider";
+import { Toast } from "@/shared/components";
 import "./App.css";
 
+function AppContent() {
+  const { toasts, removeToast } = useToastContext();
+
+  return (
+    <>
+      <AppRoutes />
+      <Toast toasts={toasts} removeToast={removeToast} />
+    </>
+  );
+}
+
 function App() {
-  return <div className="text-green-600">Hello world</div>;
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <ToastProvider>
+          <AppContent />
+        </ToastProvider>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
