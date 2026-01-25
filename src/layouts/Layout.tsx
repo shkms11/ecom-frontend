ea1283f438badcc5a1c7066d84cbc1aa4658908a
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-01-25 17:07:40 +0600

    create layout and add header and footer for a constant design

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Header } from "@/shared/components";
+import { Footer } from "@/shared/components";
+
+const Layout: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+
+      <main className="flex-1 pt-16">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
