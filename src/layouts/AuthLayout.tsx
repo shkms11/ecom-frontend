6a05adbe86a3c15064775b6cda8063a3a35b39a1
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-01-25 17:33:08 +0600

    add layouts for auth and dashboard

---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,13 @@
+import { type ReactNode } from "react";
+
+interface AuthLayoutProps {
+  children: ReactNode;
+}
+
+export const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md">{children}</div>
+    </div>
+  );
+};
