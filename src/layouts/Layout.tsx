20986bd0166238df7269faa21140c55dce6e3154
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-03-14 12:49:25 +0600

    remove top padding for outlet

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -8,7 +8,7 @@ const Layout: React.FC = () => {
     <div className="min-h-screen flex flex-col">
       <Header />
 
-      <main className="flex-1 pt-16">
+      <main className="flex-1 ">
         <Outlet />
       </main>
 
