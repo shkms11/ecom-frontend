93222526ecfdd7c2ee8aed74980ea56ddc72a5e7
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-05-07 20:26:20 +0600

    update productcard

--- src/shared/components/ProductCard.tsx
+++ src/shared/components/ProductCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import type { Product } from "@/shared/types/Product";
+import type { Product } from "@/features/products/types/product.types";
 import { formatCurrency } from "@/shared/utils";
 
 interface ProductCardProps {
