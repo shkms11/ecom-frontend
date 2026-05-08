d03c955c2913a4c3d79a4523e9623b99a0130b37
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-05-08 16:57:38 +0600

    add pages for about, category, contact, product details and shop

---
+++ src/pages/ProductDetail.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ProductDetailPage from "@/features/products/pages/ProductDetailsPage";
+
+export default function ProductDetailPageRoute() {
+  // const { id } = useParams<{ id: string }>();
+  // const product = useProductDetail(id); // your hook
+
+  return <ProductDetailPage />;
+}
---
+++ src/pages/Shop.tsx
@@ -0,0 +1,5 @@
+import ShopAllProducts from "@/features/products/pages/ShopAllProducts";
+
+export default function ShopPage() {
+  return <ShopAllProducts />;
+}
