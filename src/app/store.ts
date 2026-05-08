9abf1c0bbbdcbf23f0eb2cf680880578051ea629
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-05-08 22:30:56 +0600

    update store with productApi

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
+
 import authReducer from "@/features/auth/slices/authSlice";
+
 import { authApi } from "@/features/auth/api/authApi";
+import { productsApi } from "@/features/products/api/productsApi";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+
+    // RTK Query reducers
     [authApi.reducerPath]: authApi.reducer,
+    [productsApi.reducerPath]: productsApi.reducer,
   },
+
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: ["auth/login/pending", "auth/register/pending"],
       },
-    }).concat(authApi.middleware),
+    }).concat(authApi.middleware, productsApi.middleware),
+
   devTools: import.meta.env.DEV,
 });
 
-// Enable refetchOnFocus and refetchOnReconnect
 setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
