5f4d06616fd17f7804a2daeb85702e0c2351233d
Shamsul <48136141+shkms11@users.noreply.github.com>
2026-05-09 12:09:40 +0600

    setup shadcn and update alieis

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@/components": path.resolve(__dirname, "./src/components"),
-      "@/features": path.resolve(__dirname, "./src/features"),
-      "@/shared": path.resolve(__dirname, "./src/shared"),
-      "@/pages": path.resolve(__dirname, "./src/pages"),
-      "@/layouts": path.resolve(__dirname, "./src/layouts"),
-      "@/providers": path.resolve(__dirname, "./src/providers"),
-      "@/app": path.resolve(__dirname, "./src/app"),
-      "@/config": path.resolve(__dirname, "./src/config"),
-      "@/routes": path.resolve(__dirname, "./src/routes"),
-      "@tests": path.resolve(__dirname, "./tests"),
+      "@": path.resolve(import.meta.url, "./src"),
+      "@tests": path.resolve(import.meta.url, "./tests"),
     },
   },
 });
